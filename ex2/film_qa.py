2a4a3247e05187655ce1cde7ec5734a6ca71c1d3
Noa Leshem <>
2021-05-14 19:22:41 +0300

    Filter out films that won ealier than 2010

--- ex2/film_qa.py
+++ ex2/film_qa.py
@@ -11,11 +11,20 @@ def fetch_html(url):
 wiki_base_url = 'https://en.wikipedia.org'
 
 ## Generator that loads the wiki page of the next film in the list
-class next_film_page(object):
+class film_pages(object):
+		@staticmethod
+		def check_year(row):
+			relevant_years = [str(y) for y in range(2010, 2021)]
+			for year in relevant_years:
+				if len(row.xpath("./td[2]//*[contains(text(), '{}')]".format(year))) > 0:
+					return True
+			return False
+
 		def __init__(self):
 				self.film_list = fetch_html('https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films')
 				self.index = 0
 				self.rows = self.film_list.xpath("//span[@id='List_of_films']/following::table[1]/tbody/tr")[1:]
+				self.rows = list(filter(lambda row: self.check_year(row), self.rows))
 				self.num_films = len(self.rows)
 
 		def __iter__(self):
@@ -33,5 +42,5 @@ class next_film_page(object):
 			raise StopIteration()
 		
 if __name__ == "__main__":
-		g = next_film_page()
+		g = film_pages()
 				
